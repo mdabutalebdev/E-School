96c040e89e977b213d0803356c125f8a39eb370a
mdabutalebdev <mdabutaleb.dev@gmail.com>
2024-10-08 16:02:29 +0600

    e-school

---
+++ js/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+    theme: {
+      extend: {
+          // Color Part Start here
+        colors: {
+            
+            headerBg:"#F0FFF0",
+            ButtonBg:"#21B573",
+            ButtonBorder:"#000000",
+            h6Color:"#FD511A",
+            h3Color:"#413960",
+            h4Color:"#585859",
+            pColor:"#6C6B6B",
+            futterBg:"#282531",
+            clintColor:"#CDCDCD",
+        },
+        // Color Part Start here
+        // Container Part Start Here
+        width: {
+            headerContainer:"1170px",
+          },
+        // Container Part end Here
+        // font family start here
+       
+        fontfamily: {
+            Poppins:"'Poppins', sans-serif",
+              
+          },
+        // font family end here
+      },
+    },
+  };
